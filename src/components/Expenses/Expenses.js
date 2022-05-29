51b5b51098d3d5edeb17a5247cc4ad5a8453c9d5
SatrioBayu <sbayu875@gmail.com>
2022-05-29 13:17:52 +0700

    feat: Initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.data.map((expense) => (
+        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
